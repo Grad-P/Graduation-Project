d2eff978d13a70b778b0bf00360f9c79dace8ece
HodaRoushdy <hodaroshdy106@gmail.com>
2023-05-22 15:34:51 +0300

    front web

---
+++ apps/will/src/components/Prediction/Prediction.tsx
@@ -0,0 +1,35 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import { useState } from 'react';
+import Form from 'react-bootstrap/Form';
+export function Prediction (){
+  const [img , setImg] = useState<string>('')
+function test(x:any) {
+if (x.target.files && x.target.files[0]){
+setImg(URL.createObjectURL(x.target.files[0]))
+}else{
+    console.log('no image chosen')
+}
+}
+  return(
+<div className='prediction bg-secondary' style={{height:'100vh'}}>
+
+<form>
+<input type="file" id="myFile" name="imagename text-info" accept='image/gif, image/jpeg, image/png' onChange={test} className='bg-secondary'/>
+<Button variant="outline-light">send</Button>{' '}
+</form>
+<div style={{display:'flex', justifyContent:'space-between'}}>
+<div className='showImg mt-5 ms-5 ' style={{width:'30%' , height:'20%'  }}>
+<img src={img} className='img-fluid'/>
+</div>
+<div className='col-md-6 '>
+<img src="../../assets/Banner_signup_en.png" className='me-5 w-100'/>
+</div>
+</div>
+
+
+</div>
+  )
+}
+export default Prediction;
+
---
+++ apps/will/src/components/Result/Result.tsx
@@ -0,0 +1,29 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Image from 'react-bootstrap/Image';
+
+export default function Result(){
+  return(
+
+    <div className='result mt-5'>
+    <div className="result" style={{width:'10%', height:'10%', border:'1px solid black', display:'flex',alignItems:'center',justifyContent:'center', margin:'auto', zIndex:3333}}>
+      <p>result is negative</p>
+      <br />
+    </div>
+
+    <div className="treatment col-md-6" style={{ border:'1px solid white', display:'block', margin:'auto', padding:'30px', boxShadow:' 7px 7px #8881'}}>
+      <h3>Treatment</h3>
+    <p>There are three vaccines against monkeypox. Although supplies are currently limited, get vaccinated if it is offered to you as they provide a valuable level of protection against the disease. After you have been vaccinated, continue to take care to avoid catching and spreading monkeypox; this is because it takes several weeks to develop immunity after being vaccinated, and because we don’t yet know to what extent the vaccines protect you and if they stop you from infecting others as efficacy data in this outbreak setting is needed.
+Some countries are recommending vaccination for persons at risk. Many years of research have led to development of newer and safer vaccines for an eradicated disease called smallpox, which may also be useful for monkeypox. Two of these (MVA-BN and LC16) have been approved for prevention of monkeypox. Only people who are at risk (for example someone who has been a close contact of someone who has monkeypox) should be considered for vaccination. Mass vaccination is not recommended at this time.
+      </p>
+      </div>
+
+      <div className='bg-info mt-5'>
+          <Image src="../../assets/OCIFP90.jpg" fluid />
+        {/* <img src="../../assets/OCIFP90.jpg" className="img-fluid" /> */}
+      </div>
+    </div>
+  )
+}
+// style={{height:'5%', backgroundColor:'red'}}
+
+
